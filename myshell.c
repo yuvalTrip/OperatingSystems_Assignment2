b3e2c99f996d96fda7715326214454c8028cda94
Daniel Appel <45691773+appeldaniel1998@users.noreply.github.com>
2022-12-02 06:23:24 +0200

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -6,12 +6,17 @@
 #include <string.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int input_check(char *input);
 
 void caseDir();
 
 void caseCopy(char *src, char *dest);
+
+int singleAction(char *input);
 
 
 // the below code for pipes adapted from https://github.com/csabagabor/Basic-Shell-implementation-in-C/blob/master/shell.c
@@ -72,7 +77,8 @@ void executePiped(char **buf, int nr) {//can support up to 10 piped commands
                 close(fd[i - 1][1]);
                 close(fd[i - 1][0]);
             }
-            execvp(argv[0], argv);
+            if (strcmp(argv[0], "DIR") == 0) caseDir();
+            else execvp(argv[0], argv);
             perror("invalid input ");
             exit(1);//in case exec is not successfull, exit
         }
@@ -122,8 +128,264 @@ char **inputParser(char *string) {
     return argv;
 }
 
-int input_check(char *input) {
-    if (strcmp(input, "DIR") == 0 || strcmp(input, "DIR\n") == 0) caseDir();
+void hasCloseTriangleBrace(char *ret, char *string) {
+    *(ret - 1) = '\0';
+    char *firstArgProgram = string;
+
+    char *secondArgFilePath = ret + 2;
+    secondArgFilePath[strcspn(secondArgFilePath, "\n")] = 0;
+
+    //adapted from: https://jameshfisher.com/2017/02/06/how-do-i-use-fork-in-c/
+    pid_t pid = fork();
+    if (pid == 0) {
+        // I'm the child process
+
+        char **args = inputParser(string);
+
+        int file = open(secondArgFilePath, O_WRONLY | O_CREAT, 0777);
+        dup2(file, STDOUT_FILENO);
+        close(file);
+
+        // according to https://www.youtube.com/watch?v=iq7puCxsgHQ
+        char *path = "/bin/";
+        char *fullPath = (char *) malloc(strlen(path) + sizeof(args[0]) + 1);
+        strcpy(fullPath, path);
+        strcat(fullPath, args[0]);
+
+        char **env = {NULL};
+        if (execve(fullPath, args, env) == -1) {
+            perror("error");
+        }
+    } else {
+        wait(NULL);
+    }
+}
+
+void hasOpenTriangleBrace(char *ret, char *string) {
+    *(ret - 1) = '\0';
+    char *firstArgProgram = string;
+
+    char *secondArgFilePath = ret + 2;
+    secondArgFilePath[strcspn(secondArgFilePath, "\n")] = 0;
+
+    struct stat sb;  // Find file size in bytes
+    stat(secondArgFilePath, &sb);
+
+    int bufSize = sb.st_size;
+    char buf[bufSize];
+    int scrFile = open(secondArgFilePath, O_RDONLY);
+
+    if (scrFile > 0) { // there are things to read from the input
+        read(scrFile, buf, bufSize);
+        close(scrFile);
+        char **args = inputParser(buf);
+
+        char **fullArgs = malloc(sizeof(args) + sizeof(firstArgProgram));
+        fullArgs[0] = firstArgProgram;
+        int i = 1;
+        while (args[i - 1] != NULL) {
+            fullArgs[i] = args[i - 1];
+            i++;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            //I'm the child process
+            // according to https://www.youtube.com/watch?v=iq7puCxsgHQ
+            char *path = "/bin/";
+            char *fullPath = (char *) malloc(strlen(path) + sizeof(firstArgProgram) + 1);
+            strcpy(fullPath, path);
+            strcat(fullPath, firstArgProgram);
+
+            char **env = {NULL};
+            if (execve(fullPath, fullArgs, env) == -1) {
+                perror("error");
+            }
+        } else {
+            wait(NULL);
+        }
+    }
+}
+
+/*
+    6
+    use } and { to send and recv the output to TCP:IP4 socket.
+
+    } IP:PORT will open (client) connection to IP:port
+
+    { PORT will open server port (INADDR_ANY) and output the first client connected to stdin.
+    i.e. DIR } 127.0.0.1:3950 will open a connection to localhost and output DIR output
+*/
+
+//following 2 function were adapted from: https://www.educative.io/answers/how-to-implement-tcp-sockets-in-c
+void hasOpenCurlyBrace(char *string) { // receiver
+    string = string + 2;
+    int port = atoi(string);
+
+    int socket_desc, client_sock, client_size;
+    struct sockaddr_in server_addr, client_addr;
+    char server_message[2048], client_message[2048];
+
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
+
+    // Create socket:
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_desc < 0) {
+        printf("Error while creating socket\n");
+        exit(1);
+    }
+    printf("Socket created successfully\n");
+
+    // Set port and IP:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind to the set port and IP:
+    if (bind(socket_desc, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        printf("Couldn't bind to the port\n");
+        exit(1);
+    }
+    printf("Done with binding\n");
+
+    // Listen for clients:
+    if (listen(socket_desc, 1) < 0) {
+        printf("Error while listening\n");
+        exit(1);
+    }
+    printf("\nListening for incoming connections.....\n");
+
+    // Accept an incoming connection:
+    client_size = sizeof(client_addr);
+    client_sock = accept(socket_desc, (struct sockaddr *) &client_addr, &client_size);
+
+    if (client_sock < 0) {
+        printf("Can't accept\n");
+        exit(1);
+    }
+    printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+
+    // Receive client's message once (the command):
+    if (recv(client_sock, client_message, sizeof(client_message), 0) < 0) {
+        printf("Couldn't receive\n");
+        exit(1);
+    }
+    printf("%s", client_message);
+
+    // calculating result
+    char **parsedCommand = inputParser(client_message);
+    pid_t pid = fork();
+    char *tempFile = "temp.txt";
+    if (pid == 0) {
+        int file = open(tempFile, O_WRONLY | O_CREAT | O_RDONLY, 0777);
+        dup2(file, STDOUT_FILENO);
+        close(file);
+
+
+        //I'm the child process
+        // according to https://www.youtube.com/watch?v=iq7puCxsgHQ
+        char *path = "/bin/";
+        char *fullPath = (char *) malloc(strlen(path) + sizeof(parsedCommand[0]) + 1);
+        strcpy(fullPath, path);
+        strcat(fullPath, parsedCommand[0]);
+
+        char **env = {NULL};
+        if (execve(fullPath, parsedCommand, env) == -1) {
+            perror("error");
+        }
+    } else {
+        wait(NULL);
+    }
+
+    char messageToSend[2048];
+    int scrFile;
+    scrFile = open(tempFile, O_RDONLY);
+    if (scrFile > 0) {
+        read(scrFile, messageToSend, 2048);
+        close(scrFile);
+        remove(tempFile);
+    }
+
+    if (send(client_sock, messageToSend, strlen(messageToSend), 0) < 0) {
+        printf("Can't send\n");
+        exit(1);
+    }
+
+    // Closing the socket:
+    close(client_sock);
+    close(socket_desc);
+}
+
+void hasCloseCurlyBrace(char *string) {
+    char *curlyBrace = strstr(string, "}");
+    *(curlyBrace - 1) = '\0';
+    char *command = string;
+
+    char *ip = curlyBrace + 2;
+    char *portStr = strstr(ip, ":");
+    *portStr = '\0';
+    portStr += 1;
+
+    int port = atoi(portStr);
+
+
+    int socket_desc;
+    struct sockaddr_in server_addr;
+    char server_message[2048];
+    char *client_message = command;
+
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+
+    // Create socket:
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_desc < 0) {
+        printf("Unable to create socket\n");
+        exit(1);
+    }
+
+    printf("Socket created successfully\n");
+
+    // Set port and IP the same as server-side:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    // Send connection request to server:
+    if (connect(socket_desc, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        printf("Unable to connect\n");
+        exit(1);
+    }
+    printf("Connected with server successfully\n");
+
+    // Send the message to server:
+    if (send(socket_desc, client_message, strlen(client_message), 0) < 0) {
+        printf("Unable to send message\n");
+        exit(1);
+    }
+
+    // Receive the server's response:
+    if (recv(socket_desc, server_message, sizeof(server_message), 0) < 0) {
+        printf("Error while receiving server's msg\n");
+        exit(1);
+    }
+
+    printf("Server's response: \n%s\n", server_message);
+
+    // Close the socket:
+    close(socket_desc);
+}
+
+int singleAction(char *input) {
+    if (strcmp(input, "DIR") == 0 || strcmp(input, "DIR\n") == 0) {
+        caseDir();
+        return 0;
+    }
     else {
         char delim1[] = " ";
 
@@ -136,36 +398,62 @@ int input_check(char *input) {
             dest[strcspn(dest, "\n")] = 0;
             caseCopy(src, dest);
             return 0;
-        } else {
+        } else if (strchr(input, '|') == NULL && strchr(input, '<') == NULL && strchr(input, '>') == NULL
+                   && strchr(input, '{') == NULL && strchr(input, '}') == NULL) {
+            //adapted from: https://jameshfisher.com/2017/02/06/how-do-i-use-fork-in-c/
+            pid_t pid = fork();//create 2 process
+            if (pid == 0) {//child ID will always be 0.
+                //I'm the child process;
+                char **argv = inputParser(input);
 
-            if (strchr(input, '|') == NULL) { // The string doesn't contain a pipe
-                //adapted from: https://jameshfisher.com/2017/02/06/how-do-i-use-fork-in-c/
-                pid_t pid = fork();//create 2 process
-                if (pid == 0) {//child ID will always be 0.
-                    //I'm the child process;
-                    char **argv = inputParser(input);
+                // according to https://www.youtube.com/watch?v=iq7puCxsgHQ
+                char *path = "/bin/";
+                char *fullPath = (char *) malloc(strlen(path) + sizeof(argv[0]) + 1);
+                strcpy(fullPath, path);
+                strcat(fullPath, argv[0]);
 
-                    // according to https://www.youtube.com/watch?v=iq7puCxsgHQ
-                    char *path = "/bin/";
-                    char *fullPath = (char *) malloc(strlen(path) + sizeof(argv[0]) + 1);
-                    strcpy(fullPath, path);
-                    strcat(fullPath, argv[0]);
-
-                    char **env = {NULL};
-                    if (execve(fullPath, argv, env) ==
-                        -1) //execve will always override the process it run on, in this case it will override the child process
-                    {
-                        perror("error");
-                    }
-                } else {
-                    // I'm the parent process
-                    wait(NULL);
+                char **env = {NULL};
+                if (execve(fullPath, argv, env) ==
+                    -1) //execve will always override the process it run on, in this case it will override the child process
+                {
+                    perror("error");
                 }
-            } else { // contains pipe
-                char *buffer[2048];
-                int nr = 0;
-                tokenize_buffer(buffer,&nr,input,"|");
-                executePiped(buffer,nr);
+                return 0;
+            } else {
+                // I'm the parent process
+                wait(NULL);
+                return 0;
+            }
+        } else {
+            return 1;
+        }
+    }
+}
+
+int input_check(char *input) {
+    if (singleAction(input) == 0) return 0;
+    else if (strchr(input, '|') != NULL) { // contains pipe
+        char *buffer[2048];
+        int nr = 0;
+        tokenize_buffer(buffer, &nr, input, "|");
+        executePiped(buffer, nr);
+    } else {
+        char *retClose = strstr(input, ">");
+        char *retOpen = strstr(input, "<");
+        if (retClose || retOpen) {
+            if (retClose) {
+                hasCloseTriangleBrace(retClose, input);
+            } else { //if (retOpen)
+                hasOpenTriangleBrace(retOpen, input);
+            }
+        }
+
+        char *curlyBrace = strstr(input, "}");
+        if (input[0] == '{' || curlyBrace) {
+            if (curlyBrace) {
+                hasCloseCurlyBrace(input);
+            } else { //if (string[0] == '{')
+                hasOpenCurlyBrace(input);
             }
         }
     }
